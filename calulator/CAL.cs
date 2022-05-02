fa81b99d32342324d06f05944c551a69ae29ba79
8778saquib <saquib.nawaz009@gmail.com>
2022-05-02 16:26:49 +0530

    addsub

---
+++ calulator/CAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CAL
+{
+    public class CAL
+    {
+        public void Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
+        }
+
+        public void Sub(int num1, int num2)
+        {
+            int diff = 0;
+            if (num1 >= num2)
+            {
+                diff = num1 - num2;
+            }
+            else if (num1 <= num2)
+            {
+                diff = num2 - num1;
+            }
+            Console.WriteLine("Subtraction of {0} - {1} = {2}", num1, num2, diff);
+        }
+    }
+}
---
+++ calulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CAL obj1 = new CAL();
+
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            obj1.Addition(num1, num2);
+            obj1.Sub(num1, num2);
+            
+        }
+    }
+}
